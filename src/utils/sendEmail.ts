1093a5beb12468852e3ded0e79cd62b2ee51b54a
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-08-15 23:44:22 +0800

    Update sendEmail to use sendgrid

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,26 +1,17 @@
-import nodemailer from "nodemailer";
+const sgMail = require("@sendgrid/mail");
 
 export async function sendEmail(to: string, subject: string, html: string) {
-	let testAccount = await nodemailer.createTestAccount();
-	console.log("testAccount", testAccount);
-	let transporter = nodemailer.createTransport({
-		host: "smtp.ethereal.email",
-		port: 587,
-		secure: false, // true for 465, false for other ports
-		auth: {
-			user: "apwhqrvck7fvc2nf@ethereal.email", // generated ethereal user
-			pass: "4zgUa5NGw46nwHjKnK", // generated ethereal password
-		},
-	});
-
-	// send mail with defined transport object
-	let info = await transporter.sendMail({
-		from: '"Fred Foo 👻" <foo@example.com>', // sender address
+	sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+	const msg = {
+		from: "suh.help@gmail.com", // sender address
 		to, // list of receivers
 		subject, // Subject line
 		html,
-	});
-
-	console.log("Message sent: %s", info.messageId);
-	console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+	};
+	try {
+		await sgMail.send(msg);
+		console.log("EMAIL SENT");
+	} catch (e) {
+		console.log("ERROR SENDING MAIL: ", e.response.body);
+	}
 }
